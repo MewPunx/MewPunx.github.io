438f31bf4e85f2cb8113a3932a5384b3a2cca7ff
Mewtwo <jlefort@utem.cl>
2026-03-30 00:07:46 -0300

    add project filtering functionality

--- js/main.js
+++ js/main.js
@@ -45,3 +45,28 @@ const observer = new IntersectionObserver(entries => {
 });
 
 elements.forEach(el => observer.observe(el));
+
+// Project filter
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const cards = document.querySelectorAll(".card");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        // Active button
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const filter = button.getAttribute("data-filter");
+
+        cards.forEach(card => {
+            const category = card.getAttribute("data-category");
+
+            if (filter === "all" || category === filter) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
